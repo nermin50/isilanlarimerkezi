4240a2f64a128d553fa42eae058c24a9f7c39b2a
nermin50 <nerminsaar@gmail.com>
2021-05-29 23:03:38 +0300

    Add project files.

---
+++ Job Offers Website/Models/Job.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace Job_Offers_Website.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        [DisplayName("Job title")]
+        public string JobTitle { get; set; }
+        [DisplayName("Job Content")]
+        [AllowHtml]
+        public string JobContent { get; set; }
+        [DisplayName("İmage")]
+        public string JobImage { get; set; }
+
+        [DisplayName("Job category")]
+        public int CategoryId { get; set; }
+
+        public string UserID { get; set; }
+
+        public virtual Category Category { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
